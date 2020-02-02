3a715661be407c5934dbfccf2aa404adcc8fb9cd
Dominick Lee <dnicky2288@yahoo.com>
2020-02-01 22:17:10 -0800

    Added initial codes

---
+++ AutoFlush v1/AutoFlush/AutoFlush.ino
@@ -0,0 +1,185 @@
+//Written by Dominick Lee (dominick@makitronics.com)
+//To build this, you will need 8x Neopixels WS2812, an Arduino Nano, servo, SharpIR senosr, and a TP4056 module.
+//Adjust the flush timings as desired.
+
+#include <SharpIR.h>
+#include <Adafruit_NeoPixel.h>
+#include <Servo.h> 
+
+//SharpIR
+#define ir A0
+#define model 1080
+
+//Neopixels
+#define PIN 3
+#define NUMPIXELS 5
+
+SharpIR SharpIR(ir, model);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Servo myservo; 
+
+
+void flushToilet()
+{
+  Serial.println("Flushing");
+  pixels.setPixelColor(0, pixels.Color(0,0,150)); // Moderately bright green color.
+  pixels.setPixelColor(1, pixels.Color(0,0,150)); // Moderately bright green color.
+  pixels.setPixelColor(2, pixels.Color(0,0,150)); // Moderately bright green color.
+  pixels.setPixelColor(3, pixels.Color(0,0,150)); // Moderately bright green color.
+  pixels.setPixelColor(4, pixels.Color(0,0,150)); // Moderately bright green color.
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  myservo.write(70);  // flush down
+  delay(3500);  // wait 1.5 seconds
+  myservo.write(0);
+  delay(3500);  // wait 1.5 seconds
+  myservo.detach();  // release
+
+  
+  Serial.println("Refilling tank");
+  for(int j = 0; j < 30; j++)
+  {
+    for(int i=0;i<NUMPIXELS;i++)
+    {
+      pixels.setPixelColor(0, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(1, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(2, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(3, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(4, pixels.Color(0,0,0)); // Moderately bright green color.
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      pixels.setPixelColor(i, pixels.Color(0,0,150)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(100); // Delay for a period of time (in milliseconds).
+      Serial.println(i);
+    }
+    for(int i=(NUMPIXELS-2);i > 0;i--)
+    {
+      pixels.setPixelColor(0, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(1, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(2, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(3, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(4, pixels.Color(0,0,0)); // Moderately bright green color.
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      pixels.setPixelColor(i, pixels.Color(0,0,150)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(100); // Delay for a period of time (in milliseconds).
+      Serial.println(i);
+    }
+  }
+      //last pixel
+      pixels.setPixelColor(0, pixels.Color(0,0,150)); // Moderately bright green color.
+      pixels.setPixelColor(1, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(2, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(3, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.setPixelColor(4, pixels.Color(0,0,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(100); // Delay for a period of time (in milliseconds).
+}
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pixels.begin(); // This initializes the NeoPixel library
+  pixels.setBrightness(50);
+  
+  //capacitive module
+  pinMode(10, OUTPUT);
+  pinMode(11, INPUT);
+  pinMode(12, OUTPUT);
+  //digitalWrite(10, HIGH);
+  digitalWrite(12, LOW);
+  
+  //wait for capacitive sensor to calibrate
+  while (digitalRead(11) == HIGH)
+  {
+    delay(1000);
+  }
+  
+}  //end setup
+
+void loop() 
+{
+  
+  int dis = SharpIR.distance();  // this returns the distance to the object you're measuring
+
+  Serial.print("Mean distance: ");  // returns it to the serial monitor
+  Serial.println(dis);
+  
+  
+  
+  //Handle Automatic Detection
+  if (dis < 80)
+  {
+    Serial.println("Initial detection");
+    
+    for(int i=0;i<NUMPIXELS;i++)
+    {
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      delay(500); // Delay for a period of time (in milliseconds).
+    }
+    
+    dis = SharpIR.distance();  // this returns the distance to the object you're measuring
+    if (dis < 80)  //check
+    {
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      pixels.setPixelColor(0, pixels.Color(120,100,0)); // Moderately bright blue color
+      pixels.setPixelColor(1, pixels.Color(120,100,0)); // Moderately bright blue color
+      pixels.setPixelColor(2, pixels.Color(120,100,0)); // Moderately bright blue color
+      pixels.setPixelColor(3, pixels.Color(120,100,0)); // Moderately bright blue color
+      pixels.setPixelColor(4, pixels.Color(120,100,0)); // Moderately bright blue color
+      pixels.show(); // This sends the updated pixel color to the hardware.
+      Serial.println("Person detected");
+      
+      int timeout = 0;
+      while (timeout <= 10)
+      {
+        dis = SharpIR.distance();  // this returns the distance to the object you're measuring
+        if (dis < 80)  //person there
+        {
+          Serial.println("Person still there");
+          timeout = 0;
+          delay(200);  //wait
+          
+          //Or we can handle Manual Flush
+          if (digitalRead(11) == HIGH)
+          {
+            delay(500);
+            if (digitalRead(11) == HIGH)
+            {
+              //clear to flush
+              //timeout = 11;
+            }
+          }  //end of manual flush check
+          
+        }
+        else
+        {
+          Serial.println(timeout);
+          timeout ++;
+          delay(200);  //wait 10 of these for 2 second delay
+        }
+      }
+
+      //clear to flush
+      flushToilet();
+      
+    }
+    
+  }
+  else
+  {
+    for(int i=0;i<NUMPIXELS;i++)
+    {
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
+    
+      pixels.show(); // This sends the updated pixel color to the hardware.
+    
+      delay(100); // Delay for a period of time (in milliseconds).
+    }
+  }
+
+}  //end loop
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# Makitronics-AutoFlush
-Automatic flusher for residential flapper-style toilets
+# Makitronics AutoFlush
+Open-source Automatic hands-free flusher for residential toilets
+
+## Overview ##
+Toilets around the world are flushed every day. However, not all toilets are made equal. For example, public restrooms occasionally feature an **automatic flush mechanism** that allows for hands-free toilet flushing that is more sanitary than pushing a level or handle.
+
+At home, many people such as children or elderly may face the same hesitancy with flushing due to frequent contact with a toilet that may have germs. Additionally, it can be a hassle to perform a complete flush of the toilet (by holding the lever down for more than 6 seconds). Commercial or name-brand automatic flushing systems cost hundreds of dollars and require expert installation. Since most residential toilets have flapper-style or canister mechanism for flushing, we have created one of the world's first open-source hands-free toilet flusher.
+
+The Makitronics AutoFlush is an open-source customizable system that can easily be mounted to almost any residential toilet. It has presence-checking algorithms to detect for human presence similar to commercial automatic flushing devices. You can customize the distance of detection, minimum detection time, absence countdown, flush duration, and tank refill duration. Once configured, the device is very reliable. Long-term consistent flushing could prevent toilet clogs to a certain extent (no guarantees).
+
+## Requirements ##
+If you would like to build **AutoFlush v1**, you will need the following:
+
+- Arduino Nano
+- 5x Individual Neopixels WS2812
+- Sharp GP2Y0A21 IR sensor
+- 55G Metal Gear Servo
+- Long USB cable (length from the nearest GFCI to your toilet)
+- USB Adapter
+- AutoFlush v1 3D printed parts
+- Firmware code
+
+If you would like to build **AutoFlush v2**, you will need the following:
+
+- Heltec WiFi Kit 8
+- Sharp GP2Y0A21 IR sensor
+- 55G Metal Gear Servo
+- Long USB cable (length from the nearest GFCI to your toilet)
+- USB Adapter
+- AutoFlush v2 3D printed parts
+- Firmware code
+
+**Compatibility:** This design is intended for residential toilets that have flapper-style or canister mechanism. It is not for dual-flush, ballcock valve, top-mount flush and pressure-assist toilets!
+
+**Disclaimer:** This project provides resources AS-IS and does not guarantee any compatibility with your toilet. No warranty is provided. By building this project, you acknowledge that you have basic knowledge of electronics and agree to take responsibility for any damages or injury at your own risk. You agree that the author will not be liable for any incompatibilities, damages, or injury as a result of this project.
+
+## Tutorial ##
+This is a work in progress. Tutorials will come out soon AutoFlush v1 is very reliable and colorful but requires hard-coded configurations. For that reason, it may be depreciated soon. AutoFlush v2 is a means to provide a smaller form-factor and better user interface. 
