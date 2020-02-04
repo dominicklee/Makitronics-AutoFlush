3e9ed0134a410b4a30732d4c8c8db73875dcff4c
Dominick Lee <dnicky2288@yahoo.com>
2020-02-04 02:35:15 -0800

    Finished WiFi Manager Implementation

--- AutoFlush v2/AutoFlush_v2/AutoFlush_v2.ino
+++ AutoFlush v2/AutoFlush_v2/AutoFlush_v2.ino
@@ -23,15 +23,37 @@
  * 
  */
 
+//Needed for WiFi Config
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
 #include <Arduino.h>
+
+//Needed for any networking
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
+
+//Needed for WiFi mgr library and settings
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+
+//For time keeping
 #include <ESP8266HTTPClient.h>
 #include <TimeLib.h>
-
+//For actual toilet flushing
 #include <SharpIR.h>
 #include <Servo.h> 
 #include <U8g2lib.h>
+
+//Raw vars from Wifi Manager are declared here and written/loaded from config.json
+char distance_detection[5];
+char min_detect_duration[5];
+char flush_duration[5];
+char refill_duration[5];
+char normal_angle[5];
+char flush_angle[5];
+//flag for saving data
+bool shouldSaveConfig = false;
 
 //SharpIR
 #define ir A0
@@ -74,7 +96,8 @@ SharpIR SharpIR(ir, model);
 Servo myservo; 
 //U8G2 Contructor
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ 16, /* clock=*/ 5, /* data=*/ 4);
-ESP8266WiFiMulti WiFiMulti; //WiFi client needed to get time
+ESP8266WiFiMulti WiFiMulti;
+WiFiManager wifiManager;
 
 //Declare function prototypes
 void flushToilet();
@@ -88,21 +111,19 @@ void setTimeUsingWIFI();
 void digitalClockDisplay();
 String printDigits(int digits);
 
+//Function prototypes for WiFi Manager
+void saveConfigCallback();
+void runWiFiLoadSettings();
+
+
 void setup() 
 {
   Serial.begin(115200);
   u8g2.begin();
+  drawScreen(5);  //show splash screen
 
-  drawScreen(5);
-  for(uint8_t t = 4; t > 0; t--) {
-      Serial.printf("[SETUP] WAIT %d...\n", t);
-      Serial.flush();
-      delay(1000);
-  }
+  runWiFiLoadSettings();  //run the whole WiFi manager, connect to WiFi, and load setting vars
 
-  WiFi.mode(WIFI_STA);
-  WiFi.hostname("AutoFlush");
-  WiFiMulti.addAP(ssidName.c_str(), password.c_str());
   setTimeUsingWIFI();
   if(timeStatus()!= timeSet) 
      Serial.println("Unable to sync time with the server");
@@ -113,6 +134,16 @@ void setup()
 
 void loop() 
 {
+  if (Serial.available() > 0)   //this allows the user to perform factory reset by sending 'config'
+  {
+    String incoming = Serial.readString();
+    if (incoming == "config") 
+    {
+      Serial.println("Settings reset");
+      wifiManager.resetSettings();
+    }
+  }
+  
   int dis = SharpIR.distance();  // this returns the distance to the object you're measuring
   //Serial.print("Mean distance: ");  // returns it to the serial monitor
   //Serial.println(dis);
@@ -329,7 +360,7 @@ void setTimeUsingWIFI()
   //This sets the time using Makitronics web server. Any server that provides unix timestamp will do.
   //This is easier to use than NTP time examples and does not require you to specify the timezone.
   
-  if((WiFiMulti.run() == WL_CONNECTED)) { // wait for WiFi connection
+  if((WiFi.status() == WL_CONNECTED)) { // wait for WiFi connection
 
       HTTPClient http;
 
@@ -353,24 +384,24 @@ void setTimeUsingWIFI()
   }
   else {
     Serial.println("WiFi not connected");
-    if (WiFiMulti.status() == WL_IDLE_STATUS) { // when not connected to a network, but powered on 
+    if (WiFi.status() == WL_IDLE_STATUS) { // when not connected to a network, but powered on 
       Serial.println("WL_IDLE_STATUS");
     }
-    else if (WiFiMulti.status() == WL_NO_SSID_AVAIL) {
+    else if (WiFi.status() == WL_NO_SSID_AVAIL) {
       Serial.println("WL_NO_SSID_AVAIL");
     }
-    else if (WiFiMulti.status() == WL_CONNECT_FAILED) {
+    else if (WiFi.status() == WL_CONNECT_FAILED) {
       Serial.println("WL_CONNECT_FAILED");
     }
-    else if (WiFiMulti.status() == WL_CONNECTION_LOST) {
+    else if (WiFi.status() == WL_CONNECTION_LOST) {
       Serial.println("WL_CONNECTION_LOST");
     }
-    else if (WiFiMulti.status() == WL_DISCONNECTED) {
+    else if (WiFi.status() == WL_DISCONNECTED) {
       Serial.println("WL_DISCONNECTED");
     }
     else {
       Serial.print("unknown status: ");
-      Serial.println(WiFiMulti.status());
+      Serial.println(WiFi.status());
     }
   }
 }
@@ -396,3 +427,169 @@ String printDigits(int digits)
   result.concat(String(digits));
   return result;
 }
+
+//callback notifying us of the need to save config
+void saveConfigCallback()
+{
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+//this function is run at startup after serial begin. loads config if exists
+void runWiFiLoadSettings()
+{
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    
+    //clean FS, for testing
+    if (!SPIFFS.exists("/formatComplete.txt")) 
+    {
+      Serial.println("Please wait 30 secs for SPIFFS to be formatted");
+      SPIFFS.format();
+      Serial.println("Spiffs formatted");
+  
+      File f = SPIFFS.open("/formatComplete.txt", "w");
+      if (!f) {
+        Serial.println("file open failed");
+      } else {
+        f.println("Format Complete");
+      }
+      f.close();
+    } 
+    else 
+    {
+      Serial.println("SPIFFS is formatted. Moving along...");
+    }
+
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(distance_detection, json["distance_detection"]);
+          strcpy(min_detect_duration, json["min_detect_duration"]);
+          strcpy(flush_duration, json["flush_duration"]);
+          strcpy(refill_duration, json["refill_duration"]);
+          strcpy(normal_angle, json["normal_angle"]);
+          strcpy(flush_angle, json["flush_angle"]);
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_distance_detection("distance_detection", "Distance Detection (cm)", distance_detection, 3);
+  WiFiManagerParameter custom_min_detect_duration("min_detect_duration", "Min Detect Duration (sec)", min_detect_duration, 3);
+  WiFiManagerParameter custom_flush_duration("flush_duration", "Flush Duration (sec)", flush_duration, 3);
+  WiFiManagerParameter custom_refill_duration("refill_duration", "Refill Duration (sec)", refill_duration, 3);
+  WiFiManagerParameter custom_normal_angle("normal_angle", "Normal Angle (deg)", normal_angle, 3);
+  WiFiManagerParameter custom_flush_angle("flush_angle", "Flush Angle (deg)", flush_angle, 3);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  //WiFiManager wifiManager;
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //set static ip
+  //wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  
+  //add all your parameters here
+  wifiManager.addParameter(&custom_distance_detection);
+  wifiManager.addParameter(&custom_min_detect_duration);
+  wifiManager.addParameter(&custom_flush_duration);
+  wifiManager.addParameter(&custom_refill_duration);
+  wifiManager.addParameter(&custom_normal_angle);
+  wifiManager.addParameter(&custom_flush_angle);
+    
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("AutoFlush Config", "12345678")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("WiFi connected");
+
+  //read updated parameters
+  strcpy(distance_detection, custom_distance_detection.getValue());
+  strcpy(min_detect_duration, custom_min_detect_duration.getValue());
+  strcpy(flush_duration, custom_flush_duration.getValue());
+  strcpy(refill_duration, custom_refill_duration.getValue());
+  strcpy(normal_angle, custom_normal_angle.getValue());
+  strcpy(flush_angle, custom_flush_angle.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["distance_detection"] = distance_detection;
+    json["min_detect_duration"] = min_detect_duration;
+    json["flush_duration"] = flush_duration;
+    json["refill_duration"] = refill_duration;
+    json["normal_angle"] = normal_angle;
+    json["flush_angle"] = flush_angle;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+
+  Serial.println("local ip");
+  Serial.println(WiFi.localIP());
+
+  cmDistance = atoi(distance_detection);
+  minDetectTime = atoi(min_detect_duration);
+  flushDuration = atoi(flush_duration);
+  refillDuration = atoi(refill_duration);
+  normalAngle = atoi(normal_angle);
+  flushAngle = atoi(flush_angle);
+
+  Serial.println(cmDistance);
+  Serial.println(minDetectTime);
+  Serial.println(flushDuration);
+  Serial.println(refillDuration);
+  Serial.println(normalAngle);
+  Serial.println(flushAngle);
+  Serial.println();
+}
+
